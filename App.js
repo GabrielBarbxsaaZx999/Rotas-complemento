610f46c27d91b2567c1ef5dbd9a68899ea33b8cd
GABRIEL SO$A <125594732+GabrielBarbxsaaZx999@users.noreply.github.com>
2024-02-29 14:46:11 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,86 +1,138 @@
 import * as React from 'react';
-import { Button, View, Text, ImageBackground } from 'react-native';
+import { Button, View, Text, ImageBackground, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({ navigation }) {
-    return (
-        <ImageBackground
-            source={{ uri: 'https://preview.redd.it/kanye-west-graduation-3840x2160-v0-kjektcn4myv91.png?auto=webp&s=09ab551caf64aa606dc8720953ad6abaca36c079' }}
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center', resizeMode: 'cover' }}
-        >
-            <View style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: 20, borderRadius: 10 }}>
-                <Text style={{ fontSize: 40, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                    Amigo estou aqui
-                </Text>
-                <Button
-                    title="Gustavo"
-                    onPress={() => navigation.navigate('Gustavo')}
-                    color="black"  // Cor roxa
-                    fontWeight="bold"
-                />
-                <Button
-                    title="Vitin"
-                    onPress={() => navigation.navigate('Vitin')}
-                    color="purple"  // Cor azul
-                    fontWeight="bold"
-                />
-            </View>
-        </ImageBackground>
-    );
+  return (
+    <ImageBackground
+      source={{ uri: 'https://preview.redd.it/kanye-west-graduation-3840x2160-v0-kjektcn4myv91.png?auto=webp&s=09ab551caf64aa606dc8720953ad6abaca36c079' }}
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center', resizeMode: 'cover' }}
+    >
+      <View style={{ backgroundColor:   '#fff', padding: 20, borderRadius: 10 }}>
+        <Text style={{ fontSize: 40, fontFamily: 'Arial', fontWeight: 'bold', color: '#333', color:'black' }}>
+          5 Lugares favoritos do SENAI-Sumaré
+        </Text>
+        <Button
+          title="Jardim da parte de trás"
+          onPress={() => navigation.navigate('Jardim')}
+        />
+        <Button
+          title="Biblioteca"
+          onPress={() => navigation.navigate('Biblioteca')}
+        />
+
+        <Button
+          title="Biblioteca 2.0"
+          onPress={() => navigation.navigate('Biblioteca 2.0')}
+        />
+
+        <Button
+          title="Mesas do al mosso"
+          onPress={() => navigation.navigate('Almosso')}
+        />
+
+        <Button
+          title="Saída florida"
+          onPress={() => navigation.navigate('Saida')}
+        />
+
+      </View>
+    </ImageBackground>
+  );
 }
 
-function GustavoScreen({ navigation }) {
-    return (
-        <ImageBackground
-            source={{ uri: 'https://preview.redd.it/kanye-west-graduation-3840x2160-v0-kjektcn4myv91.png?auto=webp&s=09ab551caf64aa606dc8720953ad6abaca36c079' }}
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center', resizeMode: 'cover' }}
-        >
-            <View style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: 20, borderRadius: 10, marginBottom: 20 }}>
-                <Text style={{ fontSize: 35, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                    Gustavo tem que imitar o coringa e rir loucamente por ser CLT e pobre!
-                    <br>
-                </br>
-                   Coloquei o Gustavo porque ele ja chupou laranja comigo
-                </Text>
-                
-                <Button title="Go back" onPress={() => navigation.goBack()} />
-            </View>
-        </ImageBackground>
-    );
+function JardimScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+<Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+                   Escolhemos essa parte porque é legal e bonita!
+                  </Text>
+                  
+      <Image
+       source={require('./jardim.jpeg')}
+        style={{ width: 800, height:600  }}
+      />
+
+
+    </View>
+  );
 }
 
-function VitinScreen({ navigation }) {
-    return (
-        <ImageBackground
-            source={{ uri: 'https://preview.redd.it/kanye-west-graduation-3840x2160-v0-kjektcn4myv91.png?auto=webp&s=09ab551caf64aa606dc8720953ad6abaca36c079' }}
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center', resizeMode: 'cover' }}
-        >
-            <View style={{ backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: 20, borderRadius: 10, marginBottom: 20 }}>
-                <Text style={{ fontSize: 35, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                    Vitin tem que imitar um Pitbull latindo raivoso!
-                    <br>
-                    </br>
-                    <Text>Coloquei o Vitin por que ele ja chupou jabuticaba comigo</Text>
-                </Text>
-                <Button title="Go back" onPress={() => navigation.goBack()} />
-            </View>
-        </ImageBackground>
-    );
+function BiblioScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+                  A biblioteca tem muitas atividades legais e a interação social lá é intensa
+                  </Text>
+      <Image
+          source={require('./biblio.jpeg')}
+          style={{ width: 800, height:600  }}
+      />
+    </View>
+  );
 }
+
+function Bib2Screen() {
+  return (
+    
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+                   A biblioteca merece duas fotos porque é muito bacana
+                  </Text>
+      <Image
+        source={require('./bib2.jpeg')}
+        style={{ width: 800, height:600  }}
+      />
+    </View>
+  );
+}
+
+function AlmossoScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+                  Almoçar nessa parte é supimpa porque podemos refletir comer e desfrutar da nossa bela vida
+                  </Text>
+      <Image
+        source={require('./almosso.jpeg')}
+        style={{ width: 800, height:600  }}
+      />
+    </View>
+  );
+}
+
+function SaidaScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+                 Essa parte da saida é bonita e nos traz o pensamentos de uma das melhores partes do SENAI
+                  </Text>
+      <Image
+        source={require('./saida.jpeg')}
+        style={{ width: 800, height:600  }}
+      />
+    </View>
+  );
+}
+
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Gustavo" component={GustavoScreen} />
-                <Stack.Screen name="Vitin" component={VitinScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Jardim" component={JardimScreen} />
+        <Stack.Screen name="Biblioteca" component={BiblioScreen} />
+        <Stack.Screen name="Biblioteca 2.0" component={Bib2Screen} />
+        <Stack.Screen name="Almosso" component={AlmossoScreen} />
+        <Stack.Screen name="Saida" component={SaidaScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
